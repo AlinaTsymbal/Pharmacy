e7718652735826efc91fa60d9f3dc6ac63c283fb
Alina Tsymbal <alina28tsymbal@gmail.com>
2021-04-27 16:17:35 +0300

    Extract basket layout to separate component

---
+++ pharmacy-ui/src/components/Basket.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="basket-wrapper">
+    <div v-if="isEmpty">
+      <span>Basket is empty!</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Basket',
+  computed: {
+    isEmpty() {
+      return this.items.length === 0
+    },
+  },
+  props: {
+    items: Array,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- pharmacy-ui/src/components/Navigation.vue
+++ pharmacy-ui/src/components/Navigation.vue
@@ -1,16 +1,31 @@
 <template>
-  <a-tabs :active-key="activeKey" @change="forward">
-    <a-tab-pane key="home" tab="Home page"/>
-    <a-tab-pane key="catalog" tab="Catalog"/>
-    <a-tab-pane key="sets" tab="Remedy sets"/>
-    <a-tab-pane key="med-kits" tab="Med kits"/>
-    <a-tab-pane key="about" tab="About service"/>
-  </a-tabs>
+  <div class="navigation-wrapper">
+    <a-tabs :active-key="activeKey" @change="forward">
+      <a-tab-pane key="home" tab="Home page"/>
+      <a-tab-pane key="catalog" tab="Catalog"/>
+      <a-tab-pane key="sets" tab="Remedy sets"/>
+      <a-tab-pane key="med-kits" tab="Med kits"/>
+      <a-tab-pane key="about" tab="About service"/>
+    </a-tabs>
+    <div class="additional-links-wrapper">
+      <div class="authorization-links">
+        <a>Login/Register</a>
+      </div>
+      <a-popover title="Basket" trigger="hover">
+        <template slot="content">
+          <Basket :items="[]"/>
+        </template>
+        <a type="primary" class="basket-popup-trigger"> Hover me</a>
+      </a-popover>
+    </div>
+  </div>
 </template>
 
 <script>
+import Basket from "@/components/Basket";
 export default {
   name: 'Navigation',
+  components: {Basket},
   methods: {
     forward(key) {
       this.activeKey = key;
@@ -33,6 +48,23 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.navigation-wrapper {
+  .additional-links-wrapper {
+    display: flex;
+    flex-direction: row;
+    position: absolute;
+    right: 2rem;
+    top: 1rem;
 
+    .authorization-links {
+      display: flex;
+      flex-direction: row;
+    }
+
+    .basket-popup-trigger {
+      margin-left: 2rem;
+    }
+  }
+}
 </style>
