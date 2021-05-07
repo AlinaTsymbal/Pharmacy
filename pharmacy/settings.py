0d716cd268a1b53a9fac810a90a6c156d5e1b4f2
Alina Tsymbal <alina28tsymbal@gmail.com>
2021-05-07 13:02:27 +0300

    Fix cors errors & add fixtures dependency

--- pharmacy/settings.py
+++ pharmacy/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-#q=s3*35%)h!&uuv*vu4-gf*dt*bd%tcuazdzg8!0z40re1)46
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
